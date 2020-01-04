81b9994347ebcd5ec4e52091694acdafc006c90d
jeanettesilvas <jeanette.a.silvas@gmail.com>
2020-01-03 19:19:36 -0800

    export fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export * from './components/Abbreviation/Abbreviation'
 export * from './components/Button/Button'
+export * from './components/Media/Media'
 export * from './components/Text/Text'
 export * from './components/TextInput/TextInput'
 export * from './components/TextInputWithIcon/TextInputWithIcon'
