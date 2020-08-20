54b4aa7eeedd852aaaad0fd3d06a9ae8aba58f94
Jeanette Silvas <jeanettesilvas.dev@gmail.com>
2020-08-19 23:02:28 -0700

    Changes to Media

--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -1,13 +1,20 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
 import './Media.scss'
 
-export class Media extends PureComponent {
-  render() {
-    return (
-      <div className="media">
-        <img className="media__image" src="" alt="" />
-      </div>
-    )
-  }
+export const Media = ({ alt, className, height, src, width }) => {
+  return (
+    <div className={`media ${className}`} style={{ height, width }}>
+      <img className="media__image" src={src} alt={alt} />
+    </div>
+  )
+}
+
+Media.propTypes = {
+  alt: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  height: PropTypes.string,
+  src: PropTypes.string.isRequired,
+  width: PropTypes.string,
 }
