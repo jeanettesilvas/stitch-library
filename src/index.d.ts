20ab5b2a73849f3538a7f5b5f6c4952c20ccb546
Jeanette Silvas <jeanettesilvas.dev@gmail.com>
2021-05-25 21:08:15 -0700

    declaration file fixes

---
+++ src/index.d.ts
@@ -0,0 +1,5 @@
+export * from './components/Abbreviation/Abbreviation';
+export * from './components/Button/Button';
+export * from './components/Media/Media';
+export * from './components/Text/Text';
+export * from './components/TextInput/TextInput';
